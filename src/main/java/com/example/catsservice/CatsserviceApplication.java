102108ff8cc6437c68271d0eec23a8ee3b1fc83b
extranuts <QroWka8462>
2021-10-24 16:40:45 +0300

    Initial commit

---
+++ src/main/java/com/example/catsservice/CatsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.catsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/catsservice/CatsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
